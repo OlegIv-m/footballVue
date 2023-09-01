8006e8a726841cfae91d8c71730a15930314beda
Oleg Ivannikov <o.ivannikov@edstein.ru>
2023-09-01 14:25:47 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:OlegIv-m/football.git main:gh-pages
+git push -f https://github.com/OlegIv-m/football.git main:gh-pages
 
 cd -
